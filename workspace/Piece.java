f89e96b2aa6e900f01112ff68ef1ce1aa21bf21a
Mark Rosenbaum <57112559+markrosenbaum@users.noreply.github.com>
2025-03-10 16:28:20 +0000

    Works1!

--- workspace/Piece.java
+++ workspace/Piece.java
@@ -1,66 +1,95 @@
-
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.imageio.ImageIO;
 
-//you will need to implement two functions in this file.
 public class Piece {
-    private final boolean color;
+    private final boolean color;    
     private BufferedImage img;
-    
+
     public Piece(boolean isWhite, String img_file) {
         this.color = isWhite;
-        
+
         try {
             if (this.img == null) {
-              this.img = ImageIO.read(getClass().getResource(img_file));
+                this.img = ImageIO.read(getClass().getResource(img_file));
             }
-          } catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("File not found: " + e.getMessage());
-          }
+        }
     }
-    
-    
 
-    
     public boolean getColor() {
         return color;
     }
-    
+
     public Image getImage() {
         return img;
     }
-    
+
     public void draw(Graphics g, Square currentSquare) {
         int x = currentSquare.getX();
         int y = currentSquare.getY();
-        
         g.drawImage(this.img, x, y, null);
     }
-    
-    
-    // TO BE IMPLEMENTED!
-    //return a list of every square that is "controlled" by this piece. A square is controlled
-    //if the piece capture into it legally.
-    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
-     return null;
-    }
-    
 
-    //TO BE IMPLEMENTED!
-    //implement the move function here
-    //it's up to you how the piece moves, but at the very least the rules should be logical and it should never move off the board!
-    //returns an arraylist of squares which are legal to move to
-    //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
-    //going to score any points.
-    public ArrayList<Square> getLegalMoves(Board b, Square start){
-    	return null;
+    // Return a list of every square that is "controlled" by this piece.
+    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
+        ArrayList<Square> controlledSquares = new ArrayList<>();
+
+        int row = start.getRow();
+        int col = start.getCol();
+
+        // Check for a piece directly adjacent (1 square in any direction)
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                if (r == row && c == col) continue; 
+                if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                    Square targetSquare = board[r][c];
+                    if (targetSquare.isOccupied() && targetSquare.getOccupyingPiece().getColor() != this.color) {
+                        controlledSquares.add(targetSquare);
+                    }
+                }
+            }
+        }
+        return controlledSquares;
+    }
+
+    
+    public ArrayList<Square> getLegalMoves(Board b, Square start) {
+        ArrayList<Square> legalMoves = new ArrayList<>();
+        Square[][] board = b.getSquareArray(); 
+
+        int row = start.getRow();
+        int col = start.getCol();
+
+        // Moving backwards to any square behind an enemy piece
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                if (board[r][c].isOccupied() && board[r][c].getOccupyingPiece().getColor() != this.color) {
+                    if (isBehindEnemy(start, board[r][c], r, c)) {
+                        legalMoves.add(board[r][c]);
+                    }
+                }
+            }
+        }
+        return legalMoves;
+    }
+
+  
+    private boolean isBehindEnemy(Square start, Square enemySquare, int eRow, int eCol) {
+        int startRow = start.getRow();
+        int startCol = start.getCol();
+        
+        // For white pieces
+        if (this.color) {
+            // Move up the board for white (lower row numbers)
+            return (eRow > startRow) && (eCol == startCol);
+        } else {  // For black pieces
+            // Move down the board for black (higher row numbers)
+            return (eRow < startRow) && (eCol == startCol);
+        }
     }
 }
